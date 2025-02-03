cb2c74f184571626530e5725e3578a07a26c82b7
ilya <156397279+huipsa@users.noreply.github.com>
2025-02-03 10:04:42 +0300

    ADDED DRF-SPECTACULAR <3

--- diplom_project/urls.py
+++ diplom_project/urls.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/2.2/topics/http/urls/ Примеры: Пред
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('my_app.urls', namespace='my_app'))
+    path('api/v1/', include('my_app.urls', namespace='my_app')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
